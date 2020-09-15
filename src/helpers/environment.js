a646fbd157d45b9f7ec0793b28c0eb46b5f66b2c
corynnem <corynnem@users.noreply.github.com>
2020-09-15 12:43:21 -0400

    typo fixed it hopefully

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1' :
         APIURL = 'http://localhost:3001';
         break;
-    case 'fourofour.herokuapp.com' :
+    case 'admin-assistant-server.herokuapp.com' :
         APIURL = 'https://admin-assistant-server.herokuapp.com/'
 }
 
